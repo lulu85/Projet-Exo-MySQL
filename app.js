723414dc7bb73735775b8a6affbae69d9896e1d6
lulu <73641393+lulu85@users.noreply.github.com>
2021-02-11 18:44:32 +0100

    creer .gitignore

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mysql = require('mysql')
+
+//DotEnv
+require('dotenv').config()
+
+//MySQL
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
 
 // EJS
 app.set('view engine','ejs')
