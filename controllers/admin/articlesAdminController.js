72c686ccac4bd963e217428461f475f9e0e610ee
lulu85 <73641393+lulu85@users.noreply.github.com>
2021-02-18 16:55:05 +0100

    correction ajouter un article

--- controllers/admin/articlesAdminController.js
+++ controllers/admin/articlesAdminController.js
@@ -1,6 +1,6 @@
 exports.getArticlesAdmin = async (req, res) => {
     const articlesTotal = await querysql('SELECT COUNT(*) AS total FROM article')
-    const articlesAdmin = await querysql('SELECT article.titre, article.image, article.description, auteur.nom FROM article INNER JOIN auteur ON article.articleId = auteur.auteurID')
+    const articlesAdmin = await querysql('SELECT article.titre, article.image, article.description, auteur.nom FROM article INNER JOIN auteur ON article.auteurId = auteur.auteurID')
     res.render('admin/articlesAdmin',{articles: articlesAdmin,total : articlesTotal[0].total})
 } 
 
@@ -14,6 +14,20 @@ exports.getAddArticleAdmin = async (req,res)=>{
 
 //AJOUTER UN ARTICLE
 exports.postAddArticleAdmin = async (req,res)=>{
-    // res.render('admin/articlesAdmin')
-
+    const {imageURL} = 'https://www.hampshire.police.uk/SysSiteAssets/media/images/hampshire/hc-web-2018-news-1-sml-card.jpg.jpg' 
+    const {titre,description,auteurId} = req.body
+    //GESTION D'EXCEPTION
+    try {
+        await querysql('INSERT INTO article (titre,description,auteurId,image) VALUES ("' + titre + '","' + description + '","' + auteurId + '","' + imageURL + '");',
+        (err,result)=>{
+            if(err) {
+                res.send(err)
+            } else {
+                return res.redirect('/admin/liste-des-articles')
+            }
+        }
+        )
+    } catch(err) {
+        res.status(400).json({message:err})
+    }
 }
