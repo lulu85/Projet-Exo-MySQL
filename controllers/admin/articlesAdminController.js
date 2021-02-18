57ca9c7d8a9a3168d202d6dd4395226d4f0f11f9
lulu85 <73641393+lulu85@users.noreply.github.com>
2021-02-18 14:57:29 +0100

    creation route/controller pour view ajouter

--- controllers/admin/articlesAdminController.js
+++ controllers/admin/articlesAdminController.js
@@ -3,3 +3,16 @@ exports.getArticlesAdmin = async (req, res) => {
     const articlesAdmin = await querysql('SELECT article.titre, article.image, article.description, auteur.nom FROM article INNER JOIN auteur ON article.articleId = auteur.auteurID')
     res.render('admin/articlesAdmin',{articles: articlesAdmin,total : articlesTotal[0].total})
 } 
+
+
+//AFFICHE LE FORMULAIRE D'UN ARTICLE
+exports.getAddArticleAdmin = async (req,res)=>{
+    res.render('admin/ajouterAdmin')
+
+}
+
+//AJOUTER UN ARTICLE
+exports.postAddArticleAdmin = async (req,res)=>{
+    //res.render('admin/articlesAdmin')
+
+}
--- routes/admin/adminRoute.js
+++ routes/admin/adminRoute.js
@@ -6,7 +6,15 @@ const articlesAdminController = require('../../controllers/admin/articlesAdminCo
 //GET
 router.get('/', adminController.getAdminPage)
 
+//-----------------------------------Pages des articles---------------------------
 //PAGE ADMIN ARTICLES
 router.get('/liste-des-articles', articlesAdminController.getArticlesAdmin)
+
+//AFFICHE LE FORMULAIRE D'UN ARTICLE
+router.get('/liste-des-articles/ajouter', articlesAdminController.getAddArticleAdmin)
+
+//AJOUTER UN ARTICLE
+router.post('/liste-des-articles/ajouter', articlesAdminController.postAddArticleAdmin)
+
 
 module.exports = router
